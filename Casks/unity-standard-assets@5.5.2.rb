aac3e9e242d7f02b6a48f25e27ccde65cea9d6fa
Peter Mounce <petermounce@improbable.io>
2017-05-16 13:36:06 +0100

    Add unity 5.5.2 and 5.6.0 optional extras that are necessary for us

---
+++ Casks/unity-standard-assets@5.5.2.rb
@@ -0,0 +1,15 @@
+cask 'unity-standard-assets@5.5.2' do
+  version '5.5.2f1,3829d7f588f3'
+  sha256 'ec50a31c7a33c784c3ef0004c23fa09182ae02b29acd93270dd5ae51fac7ca94'
+
+  url "http://netstorage.unity3d.com/unity/#{version.after_comma}/MacStandardAssetsInstaller/StandardAssets-#{version.before_comma}.pkg"
+  name 'Unity Standard Assets'
+  homepage 'https://unity3d.com/unity'
+
+  depends_on cask: 'unity@5.5.2'
+
+  pkg "StandardAssets-#{version.before_comma}.pkg"
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            pkgutil: 'com.unity3d.StandardAssets'
+end
