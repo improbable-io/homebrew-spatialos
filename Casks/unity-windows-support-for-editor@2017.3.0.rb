b0b0524d552da8b5e45802810a3f56115b4fe487
Oliver Balaam <oliverbalaam@improbable.io>
2018-02-02 14:32:19 +0000

    Unity 2017.3.0

---
+++ Casks/unity-windows-support-for-editor@2017.3.0.rb
@@ -0,0 +1,14 @@
+cask 'unity-windows-support-for-editor@2017.3.0' do
+  version '2017.3.0f3,a9f86dcd79df'
+  sha256 'b4b6c254f3bcb1c5f62dac44c187e6e4615c3758a2bd570727598e460d325c98'
+
+  url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-#{version.before_comma}.pkg"
+  name 'Unity Windows Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  depends_on cask: 'unity'
+
+  pkg "UnitySetup-Windows-Support-for-Editor-#{version.before_comma}.pkg"
+
+  uninstall pkgutil: 'com.unity3d.WindowsStandaloneSupport'
+end
---
+++ Casks/unity@2017.3.0.rb
@@ -0,0 +1,13 @@
+cask 'unity@2017.3.0' do
+  version '2017.3.0f3,a9f86dcd79df'
+  sha256 '65c4a2865d4e2d01cd08358dcca1a95537a882a17288deeb4439558ddb126c77'
+
+  url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity.pkg"
+  name 'Unity Editor'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'Unity.pkg'
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            pkgutil: 'com.unity3d.UnityEditor5.x'
+end
